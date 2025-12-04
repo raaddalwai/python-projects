1baa7917f8be5aab990aefa454ed3c9cae21b4fe
raaddalwai <raaddalwai25@gmail.com>
2025-12-04 11:32:06 +0530

    Add PS13_even_or_odd

---
+++ PS13_even_or_odd/main.py
@@ -0,0 +1,9 @@
+# Input a number and print whether it is even or odd.
+
+def main():
+    num = int(input("Enter an integer: "))
+    print("Even" if num % 2 == 0 else "Odd")
+
+
+if __name__ == "__main__":
+    main()
