d1207486df0ca67565a657715148cd03da807bf3
raaddalwai <raaddalwai25@gmail.com>
2025-12-04 11:32:07 +0530

    Add PS14_replace_spaces

---
+++ PS14_replace_spaces/main.py
@@ -0,0 +1,9 @@
+# Replace all spaces in a sentence with '-'.
+
+def main():
+    sentence = input("Enter a sentence: ")
+    print(sentence.replace(' ', '-'))
+
+
+if __name__ == "__main__":
+    main()
